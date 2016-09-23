f2e4187d299f24364ea0db5a040c96a375240a01
Ullas Aithal <Ullas Aithal>
2016-09-22 21:40:10 -0500

    semicolons working

--- fork.c
+++ fork.c
@@ -1,47 +1,105 @@
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
 void execCommand(char *);
+char** evaluation (char *);
 int main(int argc, char **argv, char **envp)
+
 {
 	char s[100];
 	char **command;
-	//memset(command, '\0', 10);
+	
 	printf("$");
 	gets(s);
-	//printf("%s",s);
-	//printf("\n%d",strlen(s));
-	execCommand(s);
 	
-	//int l = strlen(*command);
-	//printf("%s", command[0]);
-	//execvp(c[0],c);
-	//execvp(c[0],c);
+	
+	command = evaluation(s);
+	
+	execCommand(command[1]);
+	
+	printf("%s",command[1]);
+	
 	return 0;
-	//printf("\n Start %d",a++);
+
+}
+char** evaluation (char *input)
+{
+	char **buffer;
+	char *b[100];
+	char *stack[100];
+	char *temp = (char*) malloc(100);
+	//memset(temp, '\0', 1);
+	//memset(buffer, '\0', 100);
+	int buf_count=0,stack_count=0;
+	int k=0,i=0,j=0;
+	for(i=0;i<strlen(input);i++)
+	{
+		switch(input[i])
+		{
+			case '(' : {
+							//push();
+							break;
+						}
+			case ')' : {
+							//pop();
+							break;
+						}
+			case ';' : {
+							temp[k] = NULL;
+							buffer[j] = (char *)malloc(100);
+							strncpy(buffer[j++],temp,strlen(temp));
+							free(temp);
+							temp = (char*) malloc(100);
+							k=0;
+							break;
+						}
+			default : 	{
+							temp[k++] = input[i];
+							break;
+						}
+							
+			
+		}
+	}
+	if(k>0)
+	{
+							temp[k] = NULL;
+							buffer[j] = (char *)malloc(100);
+							strncpy(buffer[j++],temp,strlen(temp));
+							free(temp);
+	}
+	buffer[j]=NULL;
 	
-	//if( fork() !=0)
-	//{
-	//	printf("\n In Parent");
-	//}
-	//else
-	//{
-	//	printf("\n In Child");
-	//}
+	
+		//printf("%s",buffer[0]);
+	
+	
+	//*buffer[j] = NULL;
+	
+	//printf("%s",buffer[0]);
+	
+	//execCommand(input);
+	// int l=0;
+	// for(l=0;l<j;l++)
+	// {
+		// printf("%d = %s",l,buffer[l]);
+	// }
+	return buffer;
 }
 void execCommand(char *s)
 {
 	char *command[100];
 	int i=0,j=0,k=0;
 	char temp[100];
-	memset(temp, '\0', 1000);
-	//printf("%s\n", s);
+	memset(temp, '\0', 100);
+	
 	for(i=0;i<strlen(s);i++)
 	{
 		if(s[i] == ' ')
 		{
 			
 			strcpy(command[j++],temp);
-			memset(temp, '\0', 1000);
+			memset(temp, '\0', 100);
 			k=0;
 			
 			
@@ -57,11 +115,7 @@ void execCommand(char *s)
 	}
 	command[j++] = temp;
 	command[j] = NULL;
-	//printf("%s\n", command[0]);
-	//printf("%s", command[1]);
-	//printf("%s", command[2]);
-	//printf("%s", command[3]);
-	//printf("%s", command[4]);
+	
 	execvp(command[0],command);
 	
 }
